ffd319de7f37ce83d376913af7277e3e4be6a155
unknown <wps7651@X1071447.ctl.intranet>
2013-10-21 16:40:40 -0500

    Made config file work out of the box

--- config.py
+++ config.py
@@ -3,15 +3,15 @@
 # Do not edit the tomeatyou.py script directly, instead change the configuration options in this file instead.
 
 #-------------------I/O Config vars----------------------------------
-LOGFILE = '/root/tomeatyou/logfile.log' # Define the location of the logfile
-INFILE = '/root/tomeatyou/search-text.txt' # Define the location of the search text in-file
-POSTSPAST = '/root/tomeatyou/postsPast.txt' # Define the location of the pastPosts output file
+LOGFILE = 'logfile.log' # Define the location of the logfile
+INFILE = 'search-text2.txt' # Define the location of the search text in-file
+POSTSPAST = 'postsPast.txt' # Define the location of the pastPosts output file
 #-------------------I/O Config vars----------------------------------
 
 
 #-------------------Twitter config----------------------------------
 CONSUMER_KEY='bw42b0kSIojsfrvhrpeew' # Replace with yours, this is just gibberish 
-CONSUMER_SECRET='6uU87zRdPhDlj7kCNISMHVZAsRtgHrk9xr5pkfRmA58' # Replace with yours, this is just gibberish 
-ACCESS_TOKEN_KEY='1026881880-NRFJhKKwBxxnTqJYhgVjBOF0iCM2IywVhEvwaAaa' # Replace with yours, this is just gibberish 
-ACCESS_TOKEN_SECRET='aiBk4ijeH7L7kcJEGLRhbm-SSkS1qRRYXj0qLinSc' # Replace with yours, this is just gibberish 
+CONSUMER_SECRET='6uUa7zRdPhDlj7kCNISMHVZAsRtgHrk9xr5pkfRmA58' # Replace with yours, this is just gibberish 
+ACCESS_TOKEN_KEY='10268818a0-NRFJhKKwBxxnTqJYhgVjBOF0iCM2IywVhEvwaAaa' # Replace with yours, this is just gibberish 
+ACCESS_TOKEN_SECRET='aiBk4ijeH7L7kcJEGLahbm-SSkS1qRRYXj0qLinSc' # Replace with yours, this is just gibberish 
 #-------------------Twitter config----------------------------------
